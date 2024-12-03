48c79cede80f3760ddd78a7676577b9e32b78a8b
Justus Heilingbrunner <heilingbrunner@stadtwerk.org>
2024-12-03 15:34:13 +0100

    small changes

--- app.py
+++ app.py
@@ -267,9 +267,7 @@ def main():
     with tab2:
         st.write('Als Default ist hier das Teilgebiet ausgewählt, dass die höchste Prio hat. Die restlichen Teilgebiete sind nach absteigender Prio sortiert.')
         
-        if st.button('Show Info', help='helping', icon='ℹ️'):
-            # Display information when the button is clicked
-            st.info('This is an informational message triggered by the button.')
+        st.button('Show Info', help='helping', icon='ℹ️', disabled=True)
 
         selected_option = st.selectbox("Wähle ein Teilgebiet aus:", ss['subareas'], index=0)
 
