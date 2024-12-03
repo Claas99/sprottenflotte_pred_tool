4c4b636719048a0fd6d9083bba01b85c6e856923
Claas Resow <claas.resow@dwins.de>
2024-12-03 14:41:44 +0100

    aasaf

--- app.py
+++ app.py
@@ -140,8 +140,8 @@ def measures_prio_of_subarea(stations_df:pd.DataFrame, predictions_df:pd.DataFra
     for station in stations:
         teilbereich = full_df[full_df['entityId'] == station]['subarea'].unique()[0]
         max_capacity = subareas_df[subareas_df['subarea'] == teilbereich]['maximum_capacity'].unique()[0]
-        st.write(station)
-        for pred in station['prediction_availableBikeNumber']:
+        station_data = full_df[full_df['entityId'] == station]
+        for pred in station_data['prediction_availableBikeNumber']:
             # Berechne die Differenz zwischen 
             if pred >= (0.8 * max_capacity):
                 prio += 1
