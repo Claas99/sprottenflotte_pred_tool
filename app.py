aff5174881a4f0c0101425d45f1c6ae2ba37cbe9
Claas Resow <claas.resow@dwins.de>
2024-12-06 07:54:29 +0100

    fix

--- app.py
+++ app.py
@@ -506,7 +506,7 @@ def main():
         if predictions_df is not None:
             predictions_df['time_utc'] = pd.to_datetime(predictions_df['time_utc'])
             predictions_df['deutsche_timezone'] = predictions_df['time_utc'] + pd.Timedelta(hours=1)
-            predictions_df = predictions_df.merge(station_df[['entityId', 'station_name']], on='entityId', how='left')
+            predictions_df = predictions_df.merge(stations_df[['entityId', 'station_name']], on='entityId', how='left')
             st.dataframe(predictions_df[['entityId', 'station_name', 'deutsche_timezone', 'availableBikeNumber']])
             pivot_df = predictions_df.pivot(index='station_name', columns='deutsche_timezone', values='prediction_availableBikeNumber')
             st.dataframe(pivot_df)
