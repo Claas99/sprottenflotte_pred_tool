d3a7b9328a6e39b88b94b2a60429d93a173c5538
Justus Heilingbrunner <heilingbrunner@stadtwerk.org>
2024-12-10 23:17:52 +0100

    small change

--- predictions.py
+++ predictions.py
@@ -24,8 +24,8 @@ log = logging.getLogger()
 
 ### Configurations
 DATA_FILENAME = 'data/data_temp.csv'
-MODEL_FILENAME = 'data/model.joblib' # 'data/cnn_model.pth'
-SCALER_FILENAME = 'data/scaler.joblib' # 'data/scaler.pkl'
+MODEL_FILENAME = 'data/model_rf.joblib' # 'data/cnn_model.pth'
+SCALER_FILENAME = 'data/scaler_rf.joblib' # 'data/scaler.pkl'
 PREDICTIONS_FILENAME = 'data/predictions.csv'
 GITHUB_TOKEN = st.secrets['GITHUB_TOKEN']
 NAME_REPO = "Claas99/sprottenflotte_pred_tool"
