0e3fe33c027dbba5040b6bbdd75922f8eed834b5
Justus Heilingbrunner <heilingbrunner@stadtwerk.org>
2024-12-11 10:41:46 +0100

    inserted the final model, made changes to prediction_test.py

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ def main():
         data_df, data_message_type, data_message_text = data.update_station_data()
         test_df_cool = predictions_test.make_dataframe_for_prediction_model(data_df, weather_data_df, stations_df)
         # predictions_df, pred_message_type, pred_message_text = predictions_test.update_predictions(data_df, weather_data_df, stations_df)
-        data = predictions_test.update_predictions(data_df, weather_data_df, stations_df)
-        st.dataframe(data, use_container_width=True)
+        test_data = predictions_test.update_predictions(data_df, weather_data_df, stations_df)
+        st.dataframe(test_data, use_container_width=True)
 
 
         predictions_df, pred_message_type, pred_message_text = predictions.update_predictions(data_df) # use data_df weil in der function sonst eine veraltete version von den daten eingelesen wird, wichtig bei stundenänderung
