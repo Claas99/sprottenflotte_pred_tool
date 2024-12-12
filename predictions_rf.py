b17ab106c621dca207e18e0c13e902d1e563c9e7
Justus Heilingbrunner <heilingbrunner@stadtwerk.org>
2024-12-12 14:29:27 +0100

    changed name of predition file for random forest

--- predictions_rf.py
+++ predictions_rf.py
@@ -26,7 +26,7 @@ log = logging.getLogger()
 DATA_FILENAME = 'data/data_temp.csv'
 MODEL_FILENAME = 'models/model_rf.joblib' # 'data/cnn_model.pth'
 SCALER_FILENAME = 'models/scaler_rf.joblib' # 'data/scaler.pkl'
-PREDICTIONS_FILENAME = 'data/predictions_random_forest.csv'
+PREDICTIONS_FILENAME = 'data/predictions_rf.csv'
 GITHUB_TOKEN = st.secrets['GITHUB_TOKEN']
 NAME_REPO = "Claas99/sprottenflotte_pred_tool"
 
