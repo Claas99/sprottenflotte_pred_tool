210a2adad381ace84b1757c7c186e48191fbed2e
Claas99 <135819277+Claas99@users.noreply.github.com>
2024-12-12 10:30:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def main():
         with st.spinner("Historische Daten werden geladen..."):
             data_df, data_message_type, data_message_text = data.update_station_data()
             st.toast("Historische Daten geladen", icon="🕵️‍♂️")
-        if model_selection == "Random Forest"
+        if model_selection == "Random Forest":
             with st.spinner("Predictions werden berechnet..."):
                 predictions_df, pred_message_type, pred_message_text = predictions.update_predictions(data_df) # use data_df weil in der function sonst eine veraltete version von den daten eingelesen wird, wichtig bei stundenänderung 
         else: 
